bd25f04856f6298ba8f16b4d95f67748d51e9c09
Eric Cooper <ecc@cmu.edu>
2020-05-03 00:00:03 -0400

    Include slope in trend test

--- trend_test.go
+++ trend_test.go
@@ -7,19 +7,24 @@ import (
 
 func TestTrend(t *testing.T) {
 	cases := []struct {
-		trend   string
 		entries Entries
+		slope   float64
+		trend   string
 	}{
-		{"DoubleUp", sgvEntries(126, 108, 93, 79)},
-		{"SingleUp", sgvEntries(108, 93, 79, 77)},
-		{"FortyFiveUp", sgvEntries(141, 135, 126, 121)},
-		{"Flat", sgvEntries(102, 98, 97, 99)},
-		{"FortyFiveDown", sgvEntries(82, 87, 93, 99)},
-		{"SingleDown", sgvEntries(117, 117, 129, 147)},
-		{"DoubleDown", sgvEntries(117, 129, 147, 164)},
+		{sgvEntries(126, 108, 93, 79), 3.12, "DoubleUp"},
+		{sgvEntries(108, 93, 79, 77), 2.14, "SingleUp"},
+		{sgvEntries(141, 135, 126, 121), 1.38, "FortyFiveUp"},
+		{sgvEntries(102, 98, 97, 99), 0.2, "Flat"},
+		{sgvEntries(82, 87, 93, 99), -1.14, "FortyFiveDown"},
+		{sgvEntries(117, 117, 129, 147), -2.04, "SingleDown"},
+		{sgvEntries(117, 129, 147, 164), -3.18, "DoubleDown"},
 	}
 	for _, c := range cases {
 		t.Run(c.trend, func(t *testing.T) {
+			slope := FindLine(c.entries).Slope
+			if !closeEnough(slope, c.slope) {
+				t.Errorf("Slope == %v, want %v", slope, c.slope)
+			}
 			trend := Trend(c.entries)
 			if trend != c.trend {
 				t.Errorf("Trend == %s, want %s", trend, c.trend)
